5ce441a95570cf300c7d5e71acda3bda7825f413
Oguidan <michaeloguidan@gmail.com>
2022-04-27 13:01:22 +0000

    commit

--- SIB_DRH_Tool.py
+++ SIB_DRH_Tool.py
@@ -1,10 +1,10 @@
 from kivy.lang import Builder
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.properties import ObjectProperty
-from kivymd.app import MDApp
 class GererLesListes(Screen):
     pass
-class NewGameScreen(Screen):
+class ImprimerDesFiches(Screen):
     pass
 # class LoadGameScreen(Screen):
     # pass
@@ -12,7 +12,7 @@ class ScreenManager(ScreenManager):
     pass
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "BlueGray"
         return Builder.load_file("SIB_DRH_Tool.kv")
 MainApp().run()
